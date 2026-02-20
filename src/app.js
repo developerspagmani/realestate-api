0f91aa58e81e8ae62802ab220029c6d7f686d12b
manik <manik@antarcticagency.com>
2026-02-20 17:24:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -74,10 +74,10 @@ app.use(helmet());
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  "https://app.virpanix.com/",
-  "https://realestate-api-seven.vercel.app/",
-  "http://localhost:3000",
-  "http://localhost:3001",
+  "https://app.virpanix.com",
+  "https://realestate-api-seven.vercel.app",
+  // "http://localhost:3000",
+  // "http://localhost:3001",
 ].filter(Boolean);
 
 app.use(cors({
