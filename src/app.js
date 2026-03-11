4fae084dfe0a273d6439e84f5d0f589c119b862b
developerspagmani <manikandan.r@spagylo.com>
2026-03-12 01:48:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -78,6 +78,7 @@ const allowedOrigins = [
   "https://realestate-api-seven.vercel.app",
   "http://localhost:3000",
   "http://localhost:3001",
+  "https://unsolemnised-renda-feirie.ngrok-free.dev",
   "https://realestate-admin-virpanix.vercel.app",
   "https://realestate-admin-vert.vercel.app",
 ].filter(Boolean);
